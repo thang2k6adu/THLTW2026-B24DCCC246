b3a7b3a5445470f989b0326b8e841e3e7cc26c80
thang2k6adu <thang2k6adu@gmail.com>
2026-03-11 07:30:00 +0700

    refactor: improve UI styling with Ant Design

--- src/pages/Bai1/index.tsx
+++ src/pages/Bai1/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Typography, Row, Col } from 'antd';
 import RockPaperScissors from './components/RockPaperScissors';
+
+const { Title } = Typography;
 
 const Bai1Page = () => {
   return (
-    <RockPaperScissors />
+    <div style={{ padding: 24 }}>
+      <Row justify="center">
+        <Col span={24} style={{ textAlign: 'center', marginBottom: 24 }}>
+          <Title level={2}>Hệ Thống 1: Trò Chơi Oẳn Tù Tì</Title>
+        </Col>
+      </Row>
+      <RockPaperScissors />
+    </div>
   );
 };
 
--- src/pages/Bai2/index.tsx
+++ src/pages/Bai2/index.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
-import { Space } from 'antd';
+import { Space, Typography, Row, Col } from 'antd';
 import KnowledgeBlockManager from './components/KnowledgeBlockManager';
 import SubjectManager from './components/SubjectManager';
 import QuestionManager from './components/QuestionManager';
 import ExamStructureManager from './components/ExamStructureManager';
 import SavedExamsViewer from './components/SavedExamsViewer';
 
+const { Title } = Typography;
+
 const Bai2Page = () => {
   return (
-    <Space direction="vertical" style={{ width: '100%', padding: 24 }} size="large">
-      <SavedExamsViewer />
-      <ExamStructureManager />
-      <QuestionManager />
-      <SubjectManager />
-      <KnowledgeBlockManager />
-    </Space>
+    <div style={{ padding: 24 }}>
+      <Row justify="center">
+        <Col span={24} style={{ textAlign: 'center', marginBottom: 24 }}>
+          <Title level={2}>Hệ Thống 2: Quản Lý Thi Khảo Sát</Title>
+        </Col>
+      </Row>
+      <Space direction="vertical" style={{ width: '100%' }} size="large">
+        <SavedExamsViewer />
+        <ExamStructureManager />
+        <QuestionManager />
+        <SubjectManager />
+        <KnowledgeBlockManager />
+      </Space>
+    </div>
   );
 };
 
