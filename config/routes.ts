fe3c6fee604878b40d044906f38893e0bd3355fc
thang2k6adu <thang2k6adu@gmail.com>
2026-03-25 09:25:00 +0700

    feat(route): register diploma management module routes

--- config/routes.ts
+++ config/routes.ts
@@ -1,4 +1,4 @@
-﻿export default [
+export default [
 	{
 		path: '/user',
 		layout: false,
@@ -41,6 +41,40 @@
 		name: 'TodoList',
 		icon: 'OrderedListOutlined',
 		component: './TodoList',
+	},
+
+	// QUẢN LÝ VĂN BẰNG
+	{
+		name: 'Quản Lý Văn Bằng',
+		path: '/quan-ly-van-bang',
+		icon: 'SolutionOutlined',
+		routes: [
+			{
+				name: 'Sổ Văn Bằng',
+				path: 'so-van-bang',
+				component: './QuanLyVanBang/SoVanBang',
+			},
+			{
+				name: 'Quyết Định Tốt Nghiệp',
+				path: 'quyet-dinh',
+				component: './QuanLyVanBang/QuyetDinh',
+			},
+			{
+				name: 'Cấu Hình Biểu Mẫu',
+				path: 'bieu-mau',
+				component: './QuanLyVanBang/BieuMau',
+			},
+			{
+				name: 'Thông Tin Văn Bằng',
+				path: 'thong-tin',
+				component: './QuanLyVanBang/ThongTinVanBang',
+			},
+			{
+				name: 'Tra Cứu',
+				path: 'tra-cuu',
+				component: './QuanLyVanBang/TraCuu',
+			},
+		],
 	},
 
 	// DANH MUC HE THONG
